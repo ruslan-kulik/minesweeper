c51213c4bcbdc01c9caef52caec3c837fa75b06f
ruslan-kulik <ruslan.kulik.8@gmail.com>
2025-05-28 12:09:14 +0300

    Update Styles.jsx

--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -3,12 +3,9 @@ import Button from './UI/Button/Button';
 import bg1 from '../images/bg1.png';
 import bg2 from '../images/bg2.png';
 import bg3 from '../images/bg3.jpg';
-import bg4 from '../images/bg4.png';
-import bg5 from '../images/bg5.png';
 import bg6 from '../images/bg6.jpg';
 import bg7 from '../images/bg7.jpg';
 import bg8 from '../images/bg8.jpg';
-import bg9 from '../images/bg9.png';
 
 import mine1 from '../images/mine1.png';
 import mine2 from '../images/mine2.png';
@@ -22,14 +19,12 @@ import mine9 from '../images/mine9.png';
 import mine10 from '../images/mine10.png';
 import mine11 from '../images/mine11.png';
 import mine12 from '../images/mine12.png';
-import mine13 from '../images/mine13.png';
 
 import flag1 from '../images/flag1.png';
 import flag2 from '../images/flag2.png';
 import flag3 from '../images/flag3.png';
 import flag4 from '../images/flag4.png';
 import flag5 from '../images/flag5.png';
-import flag6 from '../images/flag6.png';
 
 const Styles = ({ applyStyles }) => {
     const [styles, setStyles] = useState({ theme: 'gray', mine: 'mine1', flag: 'flag1' });
@@ -43,20 +38,16 @@ const Styles = ({ applyStyles }) => {
                     <input defaultChecked type="radio" value='gray' name='bg' />
                 </div>
                 <div className='skin'>
+                    <img src={bg1} alt="" style={{ height: '40px' }} />
+                    <input type="radio" value='bg2' name='bg' />
+                </div>
+                <div className='skin'>
                     <img src={bg2} alt="" style={{ height: '40px' }} />
                     <input type="radio" value='bg2' name='bg' />
                 </div>
                 <div className='skin'>
                     <img src={bg3} alt="" style={{ height: '40px' }} />
                     <input type="radio" value='bg3' name='bg' />
-                </div>
-                <div className='skin'>
-                    <img src={bg4} alt="" style={{ height: '40px' }} />
-                    <input type="radio" value='bg4' name='bg' />
-                </div>
-                <div className='skin'>
-                    <img src={bg5} alt="" style={{ height: '40px' }} />
-                    <input type="radio" value='bg5' name='bg' />
                 </div>
                 <div className='skin'>
                     <img src={bg6} alt="" style={{ height: '40px' }} />
@@ -69,14 +60,6 @@ const Styles = ({ applyStyles }) => {
                 <div className='skin'>
                     <img src={bg8} alt="" style={{ height: '40px' }} />
                     <input type="radio" value='bg8' name='bg' />
-                </div>
-                <div className='skin'>
-                    <img src={bg9} alt="" style={{ height: '40px' }} />
-                    <input type="radio" value='bg9' name='bg' />
-                </div>
-                <div className='skin'>
-                    <img src={bg1} alt="" style={{ height: '40px' }} />
-                    <input type="radio" value='bg1' name='bg' />
                 </div>
             </div>
 
@@ -130,10 +113,6 @@ const Styles = ({ applyStyles }) => {
                     <img src={mine12} alt="" style={{ height: '40px' }} />
                     <input type="radio" value='mine12' name='mine' />
                 </div>
-                <div className='skin'>
-                    <img src={mine13} alt="" style={{ height: '40px' }} />
-                    <input type="radio" value='mine13' name='mine' />
-                </div>
             </div>
 
 
@@ -158,10 +137,6 @@ const Styles = ({ applyStyles }) => {
                 <div className='skin'>
                     <img src={flag5} alt="" style={{ height: '40px' }} />
                     <input type="radio" value='flag5' name='flag' />
-                </div>
-                <div className='skin'>
-                    <img src={flag6} alt="" style={{ height: '40px' }} />
-                    <input type="radio" value='flag6' name='flag' />
                 </div>
             </div>
             <br />
